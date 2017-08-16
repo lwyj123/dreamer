d2ecf68a5930dd8446601bd5c0ddcbfa99f9cca8
lwio <443474713@qq.com>
2017-08-16 11:44:30 +0800

    add localstorage

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -19,24 +19,11 @@
       TodoList
     },
     data() {
+      if (!localStorage.getItem('todos')) {
+        localStorage.setItem('todos', JSON.stringify({todos: []}))
+      }
       return {
-        todos: [{
-          title: 'Todo A',
-          project: 'Project A',
-          done: false,
-        }, {
-          title: 'Todo B',
-          project: 'Project B',
-          done: true,
-        }, {
-          title: 'Todo C',
-          project: 'Project C',
-          done: false,
-        }, {
-          title: 'Todo D',
-          project: 'Project D',
-          done: false,
-        }]
+        todos: JSON.parse(localStorage.getItem('todos')).todos
       }
     },
     computed: {
